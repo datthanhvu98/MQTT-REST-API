489bf3465e01b37460d844da18001622280b08a5
datthanhvu98 <datvuthanh98@gmail.com>
2016-03-22 18:06:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = Number(process.env.PORT || 4444);
 
 server.connection({ port: port, routes: { cors: true } });
 
-var client  = mqtt.connect('mqtt://test.mosquitto.org:1883');
+var client = mqtt.connect({port : 1883 , host : '192.168.10.106' , clientId: 'hachi' , username : 'guest' , password : '123456'});
 
 var mqttPublish = function(topic, msg){
   client.publish(topic, msg, function() {
